ae0595c1ce6c86b4e4c73bf12f303455909e7a84
noritersand <noritersand@daum.net>
2016-12-09 10:24:44 +0900

    Signed-off-by: noritersand <noritersand@daum.net>

--- src/test/java/GsonTest.java
+++ src/test/java/GsonTest.java
@@ -1,8 +1,15 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 public class GsonTest {
 	@Test
 	public void testGson() {
-		  
+		JsonObject job = new JsonObject();
+		job.addProperty("key", "value");
+		
+		Assert.assertEquals("{\"key\":\"value\"}", new Gson().toJson(job));
 	}
 }
