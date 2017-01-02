b3351e89226e6d1321d37423b8d5f035f0c778e1
fixalot <fixalot@idrgroup.co.kr>
2017-01-02 20:06:40 +0900

    Signed-off-by: fixalot <fixalot@idrgroup.co.kr>

--- src/test/java/PathTest.java
+++ src/test/java/PathTest.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 
 import org.junit.Assert;
@@ -13,4 +14,19 @@ public class PathTest {
 		Assert.assertEquals("..\\dev\\git", sso.relativize(dev).toString()); // sso에서 dev로 가려면 'cd ..\dev\git'
 		Assert.assertEquals("", dev.relativize(dev).toString()); // dev에서 dev로 가려면 'cd .'
 	}
+	
+	@Test
+	public void testRelative2() {
+		Path source = new File("webapp\\upload\\temp\\").toPath();
+		Path target = new File("webapp\\upload\\temp\\201612\\28201838255.png").toPath();
+		Assert.assertEquals("201612\\28201838255.png", source.relativize(target).toString()); // dev에서 sso로 가려면 'cd ..\..\sso'
+	}
+	
+	@Test
+	public void testResolve() throws IOException {
+		Path dev = new File("c:\\dev\\git").toPath();
+		Path someFile = dev.resolve("someFile");
+		Assert.assertEquals("c:\\dev\\git\\someFile", someFile.toString());
+	}
+	
 }
