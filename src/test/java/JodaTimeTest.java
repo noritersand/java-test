b4f6a388399a006b240a2569480ae89952dd06f6
fixalot <fixalot@idrgroup.co.kr>
2017-01-05 15:17:15 +0900

    Signed-off-by: fixalot <fixalot@idrgroup.co.kr>

--- src/test/java/JodaTimeTest.java
+++ src/test/java/JodaTimeTest.java
@@ -8,6 +8,13 @@ public class JodaTimeTest {
 	private DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss:SSS");
 	
 	@Test
+	public void testNew() {
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+		String dt = DateTime.now().toString(formatter);
+		System.out.println(dt);
+	}
+	
+	@Test
 	public void testToFormatString() {
 		DateTime dt = new DateTime();
 		System.out.println("dt: " + dt);
@@ -31,5 +38,12 @@ public class JodaTimeTest {
 	public void testFromFormatString() {
 		Assert.assertEquals(new DateTime(2020, 2, 29, 23, 59, 59, 10), 
 				formatter.parseDateTime("2020-02-29 23:59:59:010"));
+	}
+	
+	@Test
+	public void testAddDate() {
+		DateTime dt = new DateTime(2020, 2, 29, 23, 59, 59, 10);
+		DateTime newInstance = dt.plusDays(2);
+		Assert.assertEquals("2020-03-02 23:59:59:010", newInstance.toString(formatter));
 	}
 }
