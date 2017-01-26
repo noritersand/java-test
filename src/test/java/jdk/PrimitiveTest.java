0f0d87a3d8a3d3e2caa73f8568c5a890e2357898
fixalot <fixalot@idrgroup.co.kr>
2017-01-26 15:06:06 +0900

    Signed-off-by: fixalot <fixalot@idrgroup.co.kr>

--- src/test/java/jdk/PrimitiveTest.java
+++ src/test/java/jdk/PrimitiveTest.java
@@ -1,33 +1,48 @@
 package jdk;
-import java.lang.Long;
-
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PrimitiveTest {
+	@SuppressWarnings("unused")
+	private static final Logger log = LoggerFactory.getLogger(PrimitiveTest.class);
+	
 	@Test
 	public void testAutoBoxing() {
 		Assert.assertEquals(1, (long) new Long(1));
 	}
 
 	@Test
+	@SuppressWarnings("unused")
+	public void testPrimitiveEscapeCharacter() {
+		Byte bt = 123;
+		byte bt2 = 123;
+		Short st = 0;
+		short st2 = 0;
+		Integer it = 12345;
+		int it2 = 12345;
+		Long lg = 1234L;
+		long lg2 = 1234l;
+		Float flt = 1234F;
+		float flt2 = 1234f;
+		Double db = 1234D;
+		double db2 = 1234d;
+	}
+	
+	@Test
 	public void testNumberRange() {
-		byte byteValue = -128;
-		byte byteValue2 = 127;
-		short shortValue = -32768;
-		short shortValue2 = 32767;
-		int intValue = -2147483648;
-		int intValue2 = 2147483647;
-		long longValue = -9223372036854775808L;
-		long longValue2 = 9223372036854775807L;
-
-		Assert.assertEquals(-128, byteValue);
-		Assert.assertEquals(127, byteValue2);
-		Assert.assertEquals(-32768, shortValue);
-		Assert.assertEquals(32767, shortValue2);
-		Assert.assertEquals(-2147483648, intValue);
-		Assert.assertEquals(2147483647, intValue2);
-		Assert.assertEquals(-9223372036854775808L, longValue);
-		Assert.assertEquals(9223372036854775807L, longValue2);
+		Assert.assertEquals(-128, Byte.MIN_VALUE);
+		Assert.assertEquals(127, Byte.MAX_VALUE);
+		Assert.assertEquals(-32768, Short.MIN_VALUE);
+		Assert.assertEquals(32767, Short.MAX_VALUE);
+		Assert.assertEquals(-2147483648, Integer.MIN_VALUE);
+		Assert.assertEquals(2147483647, Integer.MAX_VALUE);
+		Assert.assertEquals(-9223372036854775808L, Long.MIN_VALUE);
+		Assert.assertEquals(9223372036854775807L, Long.MAX_VALUE);
+		Assert.assertEquals("1.4E-45", String.valueOf(Float.MIN_VALUE));
+		Assert.assertEquals("3.4028235E38", String.valueOf(Float.MAX_VALUE));
+		Assert.assertEquals("4.9E-324", String.valueOf(Double.MIN_VALUE));
+		Assert.assertEquals("1.7976931348623157E308", String.valueOf(Double.MAX_VALUE));
 	}
 }
