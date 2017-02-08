0d1612471179d119830ad1a350dcf30fdcaa550c
fixalot <fixalot@idrgroup.co.kr>
2017-02-08 19:19:59 +0900

    Signed-off-by: fixalot <fixalot@idrgroup.co.kr>

---
+++ src/test/java/google/guava/TypeTokenTest.java
@@ -0,0 +1,27 @@
+package google.guava;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.reflect.TypeToken;
+
+public class TypeTokenTest {
+	@SuppressWarnings("unused")
+	private static final Logger log = LoggerFactory.getLogger(TypeTokenTest.class);
+	
+	@Test
+	public void test() {
+		@SuppressWarnings("serial")
+		Type listType = new TypeToken<ArrayList<MyClass>>() {}.getType();
+		Assert.assertEquals("java.util.ArrayList<google.guava.MyClass>", String.valueOf(listType));
+	}
+}
+
+class MyClass {
+	
+}
