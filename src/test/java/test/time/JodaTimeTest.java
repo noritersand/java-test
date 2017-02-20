47a99ce0ddf4f2ba92d8f48cbb87596dee19f242
fixalot <fixalot@idrgroup.co.kr>
2017-02-20 21:09:16 +0900

    Signed-off-by: fixalot <fixalot@idrgroup.co.kr>

--- src/test/java/test/time/JodaTimeTest.java
+++ src/test/java/test/time/JodaTimeTest.java
@@ -16,8 +16,6 @@ import org.slf4j.LoggerFactory;
 public class JodaTimeTest {
 	private static final Logger log = LoggerFactory.getLogger(JodaTimeTest.class);
 	
-	private DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss:SSS");
-	
 	@Test
 	public void testNow() {
 		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
@@ -28,8 +26,45 @@ public class JodaTimeTest {
 	@Test
 	public void testFromString() {
 		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd");
-		DateTime dt = formatter.parseDateTime("2016-12-05");
-		Assert.assertEquals("2016-12-05T00:00:00.000+09:00", dt.toString());
+		DateTime dt = formatter.parseDateTime("2016-02-05");
+		Assert.assertEquals("2016-02-05T00:00:00.000+09:00", dt.toString());
+	}
+	
+	@Test
+	public void testGetter() {
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS");
+		DateTime dt = formatter.parseDateTime("2016-02-15 13:40:30.123");
+
+		// 시대(ERA)
+		Assert.assertEquals(1, dt.getEra());
+		// 세기
+		Assert.assertEquals(20, dt.getCenturyOfEra());
+		// 년
+		Assert.assertEquals(2016, dt.getYear());
+		Assert.assertEquals(16, dt.getYearOfCentury());
+		Assert.assertEquals(2016, dt.getYearOfEra());
+		Assert.assertEquals(2016, dt.getWeekyear());
+		// 달
+		Assert.assertEquals(2, dt.getMonthOfYear());
+		// 주
+		Assert.assertEquals(7, dt.getWeekOfWeekyear());
+		// 지역
+		Assert.assertEquals("Asia/Seoul", dt.getZone().toString());
+		// 일
+		Assert.assertEquals(46, dt.getDayOfYear());
+		Assert.assertEquals(15, dt.getDayOfMonth());
+		Assert.assertEquals(1, dt.getDayOfWeek());
+		// 시간
+		Assert.assertEquals(13, dt.getHourOfDay());
+		// 분
+		Assert.assertEquals(820, dt.getMinuteOfDay());
+		Assert.assertEquals(40, dt.getMinuteOfHour());
+		// 초
+		Assert.assertEquals(49230, dt.getSecondOfDay());
+		Assert.assertEquals(30, dt.getSecondOfMinute());
+		// 밀리초
+		Assert.assertEquals(123, dt.getMillisOfSecond());
+		Assert.assertEquals(1455511230123L, dt.getMillis());
 	}
 	
 	@Test
@@ -65,8 +100,8 @@ public class JodaTimeTest {
 	
 	@Test
 	public void testToFormatString() {
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss:SSS");
 		DateTime dt = new DateTime();
-		
 		DateTime newDt = dt.withYear(2020).withMonthOfYear(2).withDayOfMonth(29).withHourOfDay(23)
 				.withMinuteOfHour(59).withSecondOfMinute(59).withMillisOfSecond(10);
 		
@@ -84,12 +119,14 @@ public class JodaTimeTest {
 	
 	@Test
 	public void testFromFormatString() {
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss:SSS");
 		Assert.assertEquals(new DateTime(2020, 2, 29, 23, 59, 59, 10), 
 				formatter.parseDateTime("2020-02-29 23:59:59:010"));
 	}
 	
 	@Test
 	public void testPlusDay() {
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss:SSS");
 		DateTime dt = new DateTime(2020, 2, 29, 23, 59, 59, 10);
 		DateTime newInstance = dt.plusDays(1);
 		Assert.assertEquals("2020-03-01 23:59:59:010", newInstance.toString(formatter));
@@ -97,6 +134,5 @@ public class JodaTimeTest {
 		Assert.assertEquals("2020-03-01 23:59:59:010", newInstance.toString(formatter));
 		newInstance = dt.plus(new Duration(24L*60L*60L*1000L));
 		Assert.assertEquals("2020-03-01 23:59:59:010", newInstance.toString(formatter));
-		
 	}
 }
