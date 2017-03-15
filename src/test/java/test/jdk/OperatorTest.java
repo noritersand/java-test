d820d26fabff65576c90e816e4c35ca45621decf
fixalot <fixalot@idrgroup.co.kr>
2017-03-15 15:10:37 +0900

    Signed-off-by: fixalot <fixalot@idrgroup.co.kr>

--- src/test/java/test/jdk/OperatorTest.java
+++ src/test/java/test/jdk/OperatorTest.java
@@ -18,5 +18,6 @@ public class OperatorTest {
 
 		String result = (!fourth.isEmpty()) ? fourth : (!third.isEmpty()) ? third : (!second.isEmpty()) ? second : first;
 		Assert.assertEquals("3", result);
+		
 	}
 }
