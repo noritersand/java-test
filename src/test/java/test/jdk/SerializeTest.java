e6499893051f8ef57b9c12f09dae1216bf3d1315
fixalot <fixalot@idrgroup.co.kr>
2017-03-21 17:44:40 +0900

    Signed-off-by: fixalot <fixalot@idrgroup.co.kr>

---
+++ src/test/java/test/jdk/SerializeTest.java
@@ -0,0 +1,64 @@
+package test.jdk;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.file.Paths;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// 내부 클래스를 시리얼라이즈히랴먄 외부 클래스도 시리얼라이즈 가능해야함
+public class SerializeTest implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@SuppressWarnings("unused")
+	private static final Logger log = LoggerFactory.getLogger(SerializeTest.class);
+
+	@Test
+	public void testDesirealize() throws ClassNotFoundException, IOException {
+		File file = Paths.get("src/test/resources/file/test.ser").toFile();
+
+		// some instance
+		SerializeMe instance = new SerializeMe();
+		instance.setValue("hello");
+		
+		// serialize
+		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file, false));
+		ObjectOutputStream oos = new ObjectOutputStream(bos);
+		oos.writeObject(instance);
+		bos.close();
+		oos.close();
+		
+		// deserialize
+		BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+		ObjectInputStream ois = new ObjectInputStream(bis);
+		SerializeMe obj = (SerializeMe) ois.readObject();
+		ois.close();
+		bis.close();
+		
+		// assert
+		Assert.assertEquals("hello", obj.getValue());
+	}
+
+	private class SerializeMe implements Serializable {
+		private static final long serialVersionUID = 6120061019387055537L;
+		private String value;
+
+		public String getValue() {
+			return value;
+		}
+
+		public void setValue(String value) {
+			this.value = value;
+		}
+	}
+}
