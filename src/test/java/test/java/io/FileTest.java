5e8597deeb5f62a459df1ec996feb9f329eb3508
fixalot <fixalot@idrgroup.co.kr>
2017-03-21 19:32:35 +0900

    Signed-off-by: fixalot <fixalot@idrgroup.co.kr>

--- src/test/java/test/java/io/FileTest.java
+++ src/test/java/test/java/io/FileTest.java
@@ -4,24 +4,25 @@ import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FileTest {
-//	@SuppressWarnings("unused")
+	@SuppressWarnings("unused")
 	private static final Logger log = LoggerFactory.getLogger(FileTest.class);
 
 	@Test
 	public void testNewInstance() {
-		File file = new File("/test"); // 이클립스를 실행한 드라이브의 루트
-		log.debug(String.valueOf(file.exists()));
+		File file = new File("/qweasdqweasd"); // 이클립스를 실행한 드라이브의 루트의 qweasdqweasd 폴더
+		Assert.assertFalse(file.exists());
 	}
 	
 	@Test
 	public void testNewInstanceFromPath() {
-		Path path = Paths.get("/test");
+		Path path = Paths.get("src/test/resources/file/file-write-test.txt");
 		File file = path.toFile();
-		log.debug(String.valueOf(file.exists()));
+		Assert.assertTrue(file.exists());
 	}
 }
