3e81d85aab186c5da1a599a1e90232a954d12555
fixalot <fixalot@idrgroup.co.kr>
2017-03-23 14:40:03 +0900

    Signed-off-by: fixalot <fixalot@idrgroup.co.kr>

--- src/test/java/test/jdk/generic/UncheckedTest.java
+++ src/test/java/test/jdk/generic/UncheckedTest.java
@@ -1,15 +1,8 @@
 package test.jdk.generic;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
