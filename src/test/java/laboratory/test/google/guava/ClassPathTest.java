4ff070c5f5696d09db41c541a434ac1f7f28003d
noritersand <noritersand@daum.net>
2017-04-07 10:52:06 +0900

    개구리 커밋

--- src/test/java/laboratory/test/google/guava/ClassPathTest.java
+++ src/test/java/laboratory/test/google/guava/ClassPathTest.java
@@ -2,7 +2,6 @@ package laboratory.test.google.guava;
 
 import java.io.IOException;
 
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,7 +15,7 @@ public class ClassPathTest {
 //	@SuppressWarnings("unused")
 	private static final Logger log = LoggerFactory.getLogger(ClassPathTest.class);
 
-	@Test
+//	@Test // 로그가 너무 길어져서 생략
 	public void testGetAllClasses() throws IOException {
 		ClassPath classPath = ClassPath.from(this.getClass().getClassLoader());
 		ImmutableSet<ClassInfo> classInfoSet = classPath.getAllClasses();
@@ -27,7 +26,7 @@ public class ClassPathTest {
 		}
 	}
 	
-	@Test
+//	@Test // 로그가 너무 길어져서 생략
 	public void testGetAllResources() throws IOException {
 		ClassPath classPath = ClassPath.from(this.getClass().getClassLoader());
 		ImmutableSet<ResourceInfo> resourceInfoSet = classPath.getResources();
