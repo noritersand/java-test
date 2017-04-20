9777d4315cce1469443f0131b2185e26eed0d884
noritersand <noritersand@daum.net>
2017-04-20 20:08:41 +0900

    Signed-off-by: noritersand <noritersand@daum.net>

--- src/test/java/laboratory/test/java/lang/ProcessBuilderTest.java
+++ src/test/java/laboratory/test/java/lang/ProcessBuilderTest.java
@@ -1,7 +1,6 @@
 package laboratory.test.java.lang;
 
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.lang.ProcessBuilder.Redirect;
 
 import org.junit.Test;
@@ -17,10 +16,6 @@ public class ProcessBuilderTest {
 		ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "cd \"C:\\dev\" && dir");
 		pb.redirectOutput(Redirect.INHERIT); // 얘네 안하면 콘솔 출력 안됨
 		pb.redirectError(Redirect.INHERIT);
-		Process p = pb.start();
-//		OutputStreamWriter writer = new OutputStreamWriter(p.getOutputStream(), Charset.forName("ISO-8859-1")); 
-		// writer가 사용할 케릭터셋을 ISO-8859-1로 하든, UTF-8로 하든, 아예 안하든 결과는 같음.
-		OutputStreamWriter writer = new OutputStreamWriter(p.getOutputStream());
-		writer.flush();
+		pb.start();
 	}
 }
