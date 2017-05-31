2b55863bf67a3aa557926308009715d60f0efc0a
noritersand <noritersand@daum.net>
2017-05-31 15:37:34 +0900

    Signed-off-by: noritersand <noritersand@daum.net>

--- src/test/java/laboratory/jdk/java/math/BigDecimalTest.java
+++ src/test/java/laboratory/jdk/java/math/BigDecimalTest.java
@@ -19,7 +19,7 @@ public class BigDecimalTest {
 	}
 	
 	@Test
-	public void testSum() {
+	public void testOperator() {
 		BigDecimal a = new BigDecimal("10");
 		BigDecimal b = new BigDecimal("20");
 		
@@ -27,5 +27,13 @@ public class BigDecimalTest {
 		Assert.assertEquals(new BigDecimal("10"), b.subtract(a));
 		Assert.assertEquals(new BigDecimal("200"), a.multiply(b));
 		Assert.assertEquals(new BigDecimal("2"), b.divide(a));
+	}
+	
+	@Test
+	public void testCompare() {
+		BigDecimal a = BigDecimal.ZERO;
+		Assert.assertEquals(0, a.compareTo(BigDecimal.ZERO)); //  a.compareTo(b)에서 0이면 a와 b가 같음
+		Assert.assertEquals(-1, a.compareTo(BigDecimal.ONE)); // a.compareTo(b)에서 -1이면 a가 b보다 작음
+		Assert.assertEquals(1, a.compareTo(new BigDecimal(-1))); // a.compareTo(b)에서 1이면 a가 b보다 큼
 	}
 }
