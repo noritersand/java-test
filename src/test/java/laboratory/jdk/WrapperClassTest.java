3fd97aa8ee93d9cd235bbad1eb2a7b2740cca432
noritersand <noritersand@daum.net>
2017-06-01 17:33:10 +0900

    Signed-off-by: noritersand <noritersand@daum.net>

---
+++ src/test/java/laboratory/jdk/WrapperClassTest.java
@@ -0,0 +1,21 @@
+package laboratory.jdk;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WrapperClassTest {
+	@SuppressWarnings("unused")
+	private static final Logger log = LoggerFactory.getLogger(WrapperClassTest.class);
+	
+	@Test
+	public void testCopy() {
+		final Integer a = new Integer(123);
+		Integer b = a;
+		Assert.assertEquals(123, b.intValue());
+		b = 456; // b = new Integer(456); String과 마찬가지로 새 객체가 할당된다.
+		Assert.assertEquals(123, a.intValue());
+		Assert.assertEquals(456, b.intValue());
+	}
+}
