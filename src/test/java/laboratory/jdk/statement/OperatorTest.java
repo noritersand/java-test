a854a6e7e29c7f93f92b8d94acfe5b9d30b2f807
noritersand <noritersand@daum.net>
2017-06-01 17:26:50 +0900

    i love test case

--- src/test/java/laboratory/jdk/statement/OperatorTest.java
+++ src/test/java/laboratory/jdk/statement/OperatorTest.java
@@ -10,7 +10,7 @@ public class OperatorTest {
 	private static final Logger log = LoggerFactory.getLogger(OperatorTest.class);
 	
 	@Test
-	public void testTernary1() {
+	public void testTernary() {
 		String first = "1";
 		String second = "2";
 		String third = "3";
@@ -18,5 +18,14 @@ public class OperatorTest {
 
 		String result = (!fourth.isEmpty()) ? fourth : (!third.isEmpty()) ? third : (!second.isEmpty()) ? second : first;
 		Assert.assertEquals("3", result);
+	}
+	
+	@Test
+	public void testUnary() {
+		int a = 0;
+		Assert.assertEquals(1, ++a); // 변수 반환 이전에 연산. 따라서  +1의 결과인 1
+		Assert.assertEquals(1, a);
+		Assert.assertEquals(1, a++); // 변수 반환 이후에 연산. 따라서 +1 하기 전의 결과인 1
+		Assert.assertEquals(2, a);
 	}
 }
