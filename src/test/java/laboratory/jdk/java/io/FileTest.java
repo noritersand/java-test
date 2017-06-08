8cca438faf8cf59621c34eec8173f64b093a2b02
noritersand <noritersand@daum.net>
2017-06-08 15:35:01 +0900

    Signed-off-by: noritersand <noritersand@daum.net>

--- src/test/java/laboratory/jdk/java/io/FileTest.java
+++ src/test/java/laboratory/jdk/java/io/FileTest.java
@@ -13,6 +13,12 @@ public class FileTest {
 	@SuppressWarnings("unused")
 	private static final Logger log = LoggerFactory.getLogger(FileTest.class);
 
+	/*
+	 * src/main 혹은 src/test는 로컬에서만 사용해야하는 경로다.
+	 * 이 경로들은 메이븐 개발 환경에서만 존재하는 폴더 구조이고 war나 jar로 빌드되면 존재하지 않는 경로이기 때문.
+	 * 따라서 경로빌드 후 생성될 경로를 절대 경로로 프로퍼티에 관리하는 편이 좋다.
+	 */
+	
 	@Test
 	public void testNewInstance() {
 		File file = new File("/qweasdqweasd"); // 이클립스를 실행한 드라이브의 루트의 qweasdqweasd 폴더
