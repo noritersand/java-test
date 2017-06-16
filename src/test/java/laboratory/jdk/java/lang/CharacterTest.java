19a9bd25a4c34d36cd32c13e89340e2004258e82
noritersand <noritersand@daum.net>
2017-06-16 09:43:48 +0900

    i hate yellow warnning

--- src/test/java/laboratory/jdk/java/lang/CharacterTest.java
+++ src/test/java/laboratory/jdk/java/lang/CharacterTest.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CharacterTest {
-	@SuppressWarnings("unused")
+//	@SuppressWarnings("unused")
 	private static final Logger log = LoggerFactory.getLogger(CharacterTest.class);
 	
 	@Test
