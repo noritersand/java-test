d6686e0f6428e266ce3753b35b0409ec53c6c5b2
noritersand <noritersand@daum.net>
2017-06-19 11:42:24 +0900

    Signed-off-by: noritersand <noritersand@daum.net>

--- src/test/java/laboratory/jdk/java/nio/CharsetTest.java
+++ src/test/java/laboratory/jdk/java/nio/CharsetTest.java
@@ -1,6 +1,7 @@
 package laboratory.jdk.java.nio;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
 import org.junit.Assert;
@@ -13,7 +14,7 @@ public class CharsetTest {
 	private static final Logger log = LoggerFactory.getLogger(CharsetTest.class);
 	
 	@Test
-	public void test() {
+	public void testCharset() {
 		Assert.assertTrue(Charset.isSupported("UTF-8"));
 		
 //		Charset charset = Charset.defaultCharset(); // default는 의도하지 않은 결과가 나올 수 있음.
@@ -26,5 +27,13 @@ public class CharsetTest {
 		Assert.assertTrue(charset.canEncode());
 		Assert.assertEquals("EUC-KR", charset.toString());
 		Assert.assertEquals("EUC-KR", charset.displayName(Locale.KOREA));
+	}
+	
+	@Test
+	public void getStandardCharsetName() {
+		Assert.assertEquals("US-ASCII", StandardCharsets.US_ASCII.name());
+		Assert.assertEquals("ISO-8859-1", StandardCharsets.ISO_8859_1.name());
+		Assert.assertEquals("UTF-8", StandardCharsets.UTF_8.name());
+		Assert.assertEquals("UTF-16", StandardCharsets.UTF_16.name());
 	}
 }
