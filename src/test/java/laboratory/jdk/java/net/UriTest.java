74a315bfc1ae123f24c931ec0a321839b73186eb
noritersand <noritersand@daum.net>
2017-08-21 15:08:35 +0900

    Signed-off-by: noritersand <noritersand@daum.net>

--- src/test/java/laboratory/jdk/java/net/UriTest.java
+++ src/test/java/laboratory/jdk/java/net/UriTest.java
@@ -4,26 +4,38 @@ import java.net.URI;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * 
+ * URI를 다루기 위한 타입 테스트.
+ * URI는 URL보다 상위 개념이며 인터넷 자원 뿐만이 아니라 컴퓨터 상의 모든 자원을 특정한다.
  * 
  * @since 2017-07-27
  * @author fixalot
  */
 public class UriTest {
+	@SuppressWarnings("unused")
+	private static final Logger logger = LoggerFactory.getLogger(UriTest.class);
+	
 	@Test
-	public void getUri() {
-		URI uri3 = URI.create("somefolder/somefile.png");
-		System.out.println(uri3);
-		Assert.assertEquals(null, uri3.getScheme());
+	public void create() {
+		URI uri = URI.create("somefolder/somefile.png");
+		Assert.assertEquals("somefolder/somefile.png", uri.toString());
+		Assert.assertEquals(null, uri.getScheme());
 
 		URI uri2 = URI.create("file://somefolder/somefile.png");
-		System.out.println(uri2);
+		Assert.assertEquals("file://somefolder/somefile.png", uri2.toString());
 		Assert.assertEquals("file", uri2.getScheme());
-
+		
+		URI uri3 = URI.create("http://google.com/someone?a=야");
+		Assert.assertEquals("http://google.com/someone?a=야", uri3.toString());
+		Assert.assertEquals("http", uri3.getScheme());
+	}
+	
+	@Test
+	public void getSpecificInformation() {
 		URI uri = URI.create("http://google.com/someone?a=야");
-		System.out.println(uri);
 
 		Assert.assertEquals("http", uri.getScheme());
 
