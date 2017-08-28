b968e5b8879802002b46ec6f375d38278849a234
noritersand <noritersand@daum.net>
2017-08-28 17:02:37 +0900

    connection 만들기 주석 추가

--- src/test/java/laboratory/jdk/java/sql/DataSource.java
+++ src/test/java/laboratory/jdk/java/sql/DataSource.java
@@ -15,11 +15,24 @@ public class DataSource {
 
 	public static Connection getConnection() {
 		final String url = "jdbc:oracle:thin:@localhost:1521:sid";
-		final String user = "TBSDEV";
-		final String pswd = "TBSDEV";
+//		final String url = "jdbc:mysql://localhost:3306/database";
+		final String user = "TEST";
+		final String pswd = "TEST";
 		if (connection == null) {
 			try {
-				Class.forName("oracle.jdbc.driver.OracleDriver");
+				/*
+				 * 예전에는 아래처럼 해야했지만, 인스턴스가 생성된 드라이버들이 알아서 DriverManager에 스스로를 추가하는걸로 추정됨. (java.sql.Driver의 구현체라서 이렇게 되는게 아닐까?)
+				 * 가령 com.mysql.jdbc.Driver의 경우 아래처럼 static 블럭이 작성되어 있음.:
+				 *    static {
+				 *        try {
+				 *            java.sql.DriverManager.registerDriver(new Driver());
+				 *        } catch (SQLException E) {
+				 *            throw new RuntimeException("Can't register driver!");
+				 *        }
+				 *    }
+				 * 그리고 DriverManager는 connection을 얻어올 때 위처럼 등록된 드라이버 중 데이터베이스에 적당한 드라이버를 골라서 가져오는걸로 추정된다.
+				 */
+//				Class.forName("oracle.jdbc.driver.OracleDriver");
 				connection = DriverManager.getConnection(url, user, pswd);
 			} catch (Exception e) {
 				logger.error(e.getMessage(), e);
