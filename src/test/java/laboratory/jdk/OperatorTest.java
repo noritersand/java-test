225f793aed702ea55c044fe0e54ef5cd28ac3dcf
noritersand <noritersand@daum.net>
2017-10-25 20:53:22 +0900

    Signed-off-by: noritersand <noritersand@daum.net>

--- src/test/java/laboratory/jdk/OperatorTest.java
+++ src/test/java/laboratory/jdk/OperatorTest.java
@@ -16,7 +16,7 @@ public class OperatorTest {
 	private static final Logger logger = LoggerFactory.getLogger(OperatorTest.class);
 
 	@Test
-	public void testTernary() {
+	public void ternaryOperator() {
 		String first = "1";
 		String second = "2";
 		String third = "3";
@@ -27,11 +27,22 @@ public class OperatorTest {
 	}
 
 	@Test
-	public void testUnary() {
+	public void unaryOperator() {
 		int a = 0;
 		Assert.assertEquals(1, ++a); // 변수 반환 이전에 연산. 따라서 +1의 결과인 1
 		Assert.assertEquals(1, a);
 		Assert.assertEquals(1, a++); // 변수 반환 이후에 연산. 따라서 +1 하기 전의 결과인 1
 		Assert.assertEquals(2, a);
+	}
+	
+	@Test
+	public void modulusOperator() {
+		Assert.assertEquals(0, 0 % 3);
+		Assert.assertEquals(1, 1 % 3);
+		Assert.assertEquals(2, 2 % 3);
+		Assert.assertEquals(0, 3 % 3);
+		Assert.assertEquals(1, 4 % 3);
+		Assert.assertEquals(2, 5 % 3);
+		Assert.assertEquals(0, 6 % 3);
 	}
 }
