c06052d78abb74b068a0a824115b4ad705c65e7b
noritersand <noritersand@daum.net>
2017-12-06 22:19:24 +0900

    Signed-off-by: noritersand <noritersand@daum.net>

--- src/test/java/laboratory/thirdparty/google/GsonBuilderTest.java
+++ src/test/java/laboratory/thirdparty/google/GsonBuilderTest.java
@@ -2,6 +2,8 @@ package laboratory.thirdparty.google;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import laboratory.util.json.GsonBuilder;
 
@@ -12,6 +14,9 @@ import laboratory.util.json.GsonBuilder;
  * @since 2017-12-01
  */
 public class GsonBuilderTest {
+	@SuppressWarnings("unused")
+	private static final Logger logger = LoggerFactory.getLogger(GsonBuilderTest.class);
+	
 	@Test
 	public void test() {
 		GsonBuilder gsonBuilder = new GsonBuilder();
