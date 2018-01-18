c16e7394f0085e450469a6c7c1066ef49d2a0c66
fixalot <fixalot@eplatz.co.kr>
2018-01-18 13:57:53 +0900

    adding test case

--- src/test/java/etc/ExceptionTest.java
+++ src/test/java/etc/ExceptionTest.java
@@ -1,5 +1,6 @@
 package etc;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +19,26 @@ public class ExceptionTest {
 		try {
 			throw new RuntimeException("for test");
 		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
+			logger.debug("I'll catch you so much!");
+		}
+	}
+
+	@Test
+	public void shouldBeNull() {
+		/*
+		 * 에러 메시지를 설정하지 않으면 에러 메시지가 null로 찍힘.
+		 * NullPointerException과는 관계 없으나 혼동할 가능성이 있다.
+		 * 
+		 * logger.error(e.getMessage(), e); 
+		 * ->
+		 * [ERROR] shouldBeNull: null
+		 * java.lang.RuntimeException: null
+		 * 			at etc.ExceptionTest.shouldBeNull(ExceptionTest.java:31)
+		 */
+		try {
+			throw new RuntimeException();
+		} catch (Exception e) {
+			Assert.assertNull(e.getMessage());
 		}
 	}
 }
