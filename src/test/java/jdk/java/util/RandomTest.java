9971d2b3e3b51bee01f0756e7f28534b291e3b12
noritersand <noritersand@daum.net>
2018-01-23 16:59:11 +0900

    add test case
    Signed-off-by: noritersand <noritersand@daum.net>

--- src/test/java/jdk/java/util/RandomTest.java
+++ src/test/java/jdk/java/util/RandomTest.java
@@ -1,6 +1,7 @@
 package jdk.java.util;
 
 import java.util.Random;
+import java.util.stream.IntStream;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,11 +15,11 @@ import org.slf4j.LoggerFactory;
  * @author fixalot
  */
 public class RandomTest {
-	@SuppressWarnings("unused")
+//	@SuppressWarnings("unused")
 	private static final Logger logger = LoggerFactory.getLogger(RandomTest.class);
 
 	@Test
-	public void test() {
+	public void testNextInt() {
 		for (int i = 0; i < 1000; ++i) {
 			Random random = new Random();
 			int num = random.nextInt(); // int의 최솟값 ~ 최댓값 사이에서 랜덤값 추출
@@ -26,5 +27,14 @@ public class RandomTest {
 			num = random.nextInt(1000); // 0부터 1000 사이에서 랜덤값 추출
 			Assert.assertTrue(0 <= num && num <= 1000);
 		}
+	}
+	
+	@Test
+	public void testInts() {
+		Random random = new Random();
+		IntStream a = random.ints();
+		logger.debug(String.valueOf(a.count()));
+		a = random.ints(100);
+		logger.debug(String.valueOf(a.count()));
 	}
 }
