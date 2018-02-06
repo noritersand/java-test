5170235da1990c946a94e9a41cc21b30ed4735d8
noritersand <noritersand@daum.net>
2018-02-06 15:11:42 +0900

    Signed-off-by: noritersand <noritersand@daum.net>

--- src/test/java/jdk/statements/SwitchTest.java
+++ src/test/java/jdk/statements/SwitchTest.java
@@ -14,15 +14,21 @@ import org.slf4j.LoggerFactory;
 public class SwitchTest {
 	private static final Logger logger = LoggerFactory.getLogger(SwitchTest.class);
 
+	/**
+	 * switch 안의 로컬 변수는 마치 자바스크립트의 끌어올림처럼 작동함.
+	 * 
+	 * @author fixalot
+	 */
 	@Test
-	public void howVariableInitialize() {
+	public void whenVariableInitialize() {
 		int a = 2;
 		switch (a) {
 		case 1:
 			int b = 0; // 여기는 오지 않는걸로 보이지만
 			logger.debug(String.valueOf(b));
 		case 2:
-			b = 3; // 컴파일하면 초기화를 해주는걸로 바뀌나보다
+//			b; // 이렇게 하면 컴파일 에러: Syntax error, insert "VariableDeclarators" to complete LocalVariableDeclaration
+			b = 3;
 			logger.debug(String.valueOf(b));
 			Assert.assertEquals(3, b);
 		}
