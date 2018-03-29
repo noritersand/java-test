1766f265b2091bb598cc43771abdeaf0cad82121
noritersand <noritersand@gmail.com>
2018-03-29 17:47:01 +0900

    add test cases

--- src/test/java/jdk/java/math/BigDecimalTest.java
+++ src/test/java/jdk/java/math/BigDecimalTest.java
@@ -1,6 +1,7 @@
 package jdk.java.math;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -41,5 +42,53 @@ public class BigDecimalTest {
 		Assert.assertEquals(0, a.compareTo(BigDecimal.ZERO)); // a.compareTo(b)에서 0이면 a와 b가 같음
 		Assert.assertEquals(-1, a.compareTo(BigDecimal.ONE)); // a.compareTo(b)에서 -1이면 a가 b보다 작음
 		Assert.assertEquals(1, a.compareTo(new BigDecimal(-1))); // a.compareTo(b)에서 1이면 a가 b보다 큼
+	}
+	
+	/**
+	 * BigDecimal 반올림
+	 * 
+	 * @author fixalot
+	 */
+	@Test
+	public void roundDecimalPoint() {
+		BigDecimal floatNumber = new BigDecimal("12.536512304");
+		BigDecimal roundedNumber = floatNumber.setScale(2, RoundingMode.HALF_UP);
+		Assert.assertEquals(new BigDecimal("12.54"), roundedNumber);
+		roundedNumber = floatNumber.setScale(3, RoundingMode.HALF_UP);
+		Assert.assertEquals(new BigDecimal("12.537"), roundedNumber);
+		roundedNumber = floatNumber.setScale(4, RoundingMode.HALF_UP);
+		Assert.assertEquals(new BigDecimal("12.5365"), roundedNumber);
+	}
+	
+	/**
+	 * BigDecimal 올림
+	 * 
+	 * @author fixalot
+	 */
+	@Test
+	public void ceilDecimalPoint() {
+		BigDecimal floatNumber = new BigDecimal("12.536512304");
+		BigDecimal roundedNumber = floatNumber.setScale(2, RoundingMode.CEILING);
+		Assert.assertEquals(new BigDecimal("12.54"), roundedNumber);
+		roundedNumber = floatNumber.setScale(3, RoundingMode.CEILING);
+		Assert.assertEquals(new BigDecimal("12.537"), roundedNumber);
+		roundedNumber = floatNumber.setScale(4, RoundingMode.CEILING);
+		Assert.assertEquals(new BigDecimal("12.5366"), roundedNumber);
+	}
+	
+	/**
+	 * BigDecimal 내림(버림)
+	 * 
+	 * @author fixalot
+	 */
+	@Test
+	public void floorDecimalPoint() {
+		BigDecimal floatNumber = new BigDecimal("12.536512304");
+		BigDecimal roundedNumber = floatNumber.setScale(2, RoundingMode.FLOOR);
+		Assert.assertEquals(new BigDecimal("12.53"), roundedNumber);
+		roundedNumber = floatNumber.setScale(3, RoundingMode.FLOOR);
+		Assert.assertEquals(new BigDecimal("12.536"), roundedNumber);
+		roundedNumber = floatNumber.setScale(4, RoundingMode.FLOOR);
+		Assert.assertEquals(new BigDecimal("12.5365"), roundedNumber);
 	}
 }
