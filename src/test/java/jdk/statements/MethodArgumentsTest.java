38600e021e656c9f78454b5fab67911af7b3b625
noritersand <noritersand@gmail.com>
2018-04-03 10:49:29 +0900

    add test unit

---
+++ src/test/java/jdk/statements/MethodArgumentsTest.java
@@ -0,0 +1,26 @@
+package jdk.statements;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Java unlimited method arguments test case
+ * 
+ * @since 2018-04-03
+ * @author fixalot
+ */
+public class MethodArgumentsTest {
+	private static final Logger logger = LoggerFactory.getLogger(MethodArgumentsTest.class);
+
+	public static void testMe(int... args) {
+		for (int ele : args) {
+			logger.debug(String.valueOf(ele));
+		}
+	}
+	
+	@Test
+	public void test() {
+		MethodArgumentsTest.testMe(1, 2, 3, 4, 5);
+	}
+}
