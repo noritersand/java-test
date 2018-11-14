fb1523dd48ffc2813e88b04d22ce1a7899bfb275
noritersand <noritersand@gmail.com>
2018-11-14 18:02:28 +0900

    update

---
+++ src/main/java/laboratory/forblog/NoneLowercaseFilenameFinder.java
@@ -0,0 +1,37 @@
+package laboratory.forblog;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NoneLowercaseFilenameFinder {
+	private static final Logger logger = LoggerFactory.getLogger(NoneLowercaseFilenameFinder.class);
+
+	private static final String targetLocation = "C:\\dev\\git\\noritersand.github.io\\_drafts";
+	private static final String targetLocation2 = "C:\\dev\\git\\noritersand.github.io\\_posts";
+
+    public static void main(String[] args) throws Exception {
+        find(targetLocation);
+        find(targetLocation2);
+    }
+    
+    private static void find(String path) throws IOException {
+    	Path targetPath = new File(path).toPath();
+        Stream<Path> targetList = Files.list(targetPath);
+        
+        targetList.forEach((ele) -> {
+            final String fileName = ele.getFileName().toString();
+            
+            if (!fileName.equals(fileName.toLowerCase())) {
+            	logger.debug(fileName);
+            }
+        });
+
+        targetList.close();
+    }
+}
