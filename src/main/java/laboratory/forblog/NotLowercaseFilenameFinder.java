48a44c53288bb9e7554eb32d5e38cca69642ef54
noritersand <noritersand@gmail.com>
2018-12-09 00:52:24 +0900

    add comment

--- src/main/java/laboratory/forblog/NotLowercaseFilenameFinder.java
+++ src/main/java/laboratory/forblog/NotLowercaseFilenameFinder.java
@@ -9,13 +9,19 @@ import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * 깃허브-아톰-지킬 블로그 프로젝트의 _drafts와 _posts 폴더 아래에 대문자로 된 파일이 있는지 검사.
+ * 
+ * @since 2018-12-09
+ * @author fixalot
+ */
 public class NotLowercaseFilenameFinder {
 	private static final Logger logger = LoggerFactory.getLogger(NotLowercaseFilenameFinder.class);
 
 	public static void main(String[] args) throws Exception {
 		String[] targetLocations = { 
-				"C:\\\\dev\\\\git\\\\noritersand.github.io\\\\_drafts", 
-				"C:\\\\dev\\\\git\\\\noritersand.github.io\\\\_posts" 
+				"C:/dev/git/noritersand.github.io/_drafts", 
+				"C:/dev/git/noritersand.github.io/_posts" 
 				};
 
 		for (String ele : targetLocations) {
