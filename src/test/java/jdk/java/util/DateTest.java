110760417bec6df7900ae8b66a93fd0a52ac0188
removealot <fixalot@eplatz.co.kr>
2019-01-10 18:34:51 +0900

    daily update

--- src/test/java/jdk/java/util/DateTest.java
+++ src/test/java/jdk/java/util/DateTest.java
@@ -2,6 +2,7 @@ package jdk.java.util;
 
 import java.util.Date;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,8 +18,18 @@ public class DateTest {
 	private static final Logger logger = LoggerFactory.getLogger(DateTest.class);
 
 	@Test
-	public void test() {
+	public void testJavaUtilDate() {
 		Date now = new Date();
+		logger.debug(String.valueOf(now));
+	}
+	
+	@Test
+	public void testJavaSqlDate() {
+		long mills = System.currentTimeMillis();
+		logger.debug("currentTimeMills: {}", mills);
+		
+		java.sql.Date now = new java.sql.Date(mills);
+		Assert.assertEquals(mills, now.getTime());
 		logger.debug(String.valueOf(now));
 	}
 }
