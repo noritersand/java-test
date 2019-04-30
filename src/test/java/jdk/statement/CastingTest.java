29ab9a9514106e4be0d8334dd3597fe18721efe1
noritersand <noritersand@gmail.com>
2019-04-30 11:33:18 +0900

    add test unit

--- src/test/java/jdk/statement/CastingTest.java
+++ src/test/java/jdk/statement/CastingTest.java
@@ -6,17 +6,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * 
+ * 캐스팅 테스트 유닛
  * 
  * @since 2017-07-27
  * @author fixalot
  */
 public class CastingTest {
-	@SuppressWarnings("unused")
 	private static final Logger logger = LoggerFactory.getLogger(CastingTest.class);
 
 	@Test
-	public void castFromNull() {
+	public void castNullToString() {
 		Object imnotnotnull = null;
 		String string = (String) imnotnotnull;
 		Assert.assertNull(string);
@@ -24,11 +23,45 @@ public class CastingTest {
 
 	@Test
 	public void upCasting() {
-
+		Child c = new Child();
+		Parent p = c;
+		Assert.assertEquals("Child", p.getName());
+//		p.play(); // undefined method
 	}
 
 	@Test
 	public void downCasting() {
-
+		// 요로케는 안됨.
+		try {
+			Parent p = new Parent();
+			@SuppressWarnings("unused")
+			Child c = (Child) p;
+		} catch (ClassCastException e) {
+			logger.debug("캐스팅 익셉션 발생함.");
+		}
+		
+		// 요로케는 됨.
+		Parent p = new Child();
+		Child c = (Child) p;
+		
+		Assert.assertEquals("Child", c.getName());
+		c.play();
+	}
+	
+	private class Parent {
+		public String getName() {
+			return "Parent";
+		}
+	}
+	
+	private class Child extends Parent {
+		public void play() {
+			logger.debug("uoo-hoo!");
+		}
+		
+		@Override
+		public String getName() {
+			return "Child";
+		}
 	}
 }
