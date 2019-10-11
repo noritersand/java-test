cbb186c7a5004752eb8755760f66198a76fd867f
noritersand <noritersand@gmail.com>
2019-10-11 13:31:40 +0900

    little improve

--- src/main/java/laboratory/work/file/FileNameReplacer.java
+++ src/main/java/laboratory/work/file/FileNameReplacer.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
@@ -12,41 +14,77 @@ import org.slf4j.LoggerFactory;
 
 public class FileNameReplacer {
 	private static final Logger logger = LoggerFactory.getLogger(FileNameReplacer.class);
-	
-    private static final String targetLocation = "c:/dev/temp/target";
-    private static final String prefix = "02-alarm-";
-    private static final String suffix = "";
-    private static final String destLocation = "c:/dev/temp/result";
 
-    public static void main(String[] args) throws Exception {
-        Path path = new File(targetLocation).toPath();
-        Stream<Path> list = Files.list(path);
+	private static final String targetLocation = "C:\\Users\\norit\\Downloads\\wallpaper";
+	private static final String destLocation = "C:\\Users\\norit\\Downloads\\wallpaper\\temp";
+	private static final String removeMe = "wallpaper-warhammer40k-";
+	private static final String prefix;
+	static {
+		LocalDate today = LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+		final String yyyyMmdd = today.format(formatter);
+		
+		prefix = "wallpaper" + "-" + yyyyMmdd + "-";
+	}
+//	private static final String suffix = "";
 
-        // list.forEach(System.out::println);
+	public static void main(String[] args) throws Exception {
+		Path path = new File(targetLocation).toPath();
+		Stream<Path> list = Files.list(path);
 
-        File newPathParent = new File(destLocation);
-        if (!newPathParent.exists()) {
-        	newPathParent.mkdirs();
-        }
-        
-        list.forEach((k) -> {
-            final String fileName = k.toString();
-            final String newFileName = prefix
-                    + fileName.substring(fileName.lastIndexOf(File.separator) + 1)
-                    + suffix;
-            Path newPath = new File(destLocation, newFileName).toPath();
-            
-            // logging
-            logger.debug("{} -> {}", k.toString(), newPath.toString());
-            
-            try {
-                Files.move(k, newPath, StandardCopyOption.REPLACE_EXISTING);
-            } catch (IOException e) {
-                System.err.println(e);
-                System.exit(0);
-            }
-        });
+		// list.forEach(System.out::println);
 
-        list.close();
-    }
+		File newPathParent = new File(destLocation);
+		if (!newPathParent.exists()) {
+			newPathParent.mkdirs();
+		}
+
+		addPrefix(list);
+//		removePrefix(list);
+
+		list.close();
+	}
+
+	@SuppressWarnings("unused")
+	private static void removePrefix(Stream<Path> list) {		
+		list.forEach((k) -> {
+			if (!Files.isDirectory(k)) {
+				final String fileName = k.toString();
+				final String newFileName = (fileName.substring(fileName.lastIndexOf(File.separator) + 1)).replaceFirst(removeMe, "");
+				Path newPath = new File(destLocation, newFileName).toPath();
+
+				// logging
+				logger.debug("{} -> {}", k.toString(), newPath.toString());
+
+				try {
+					Files.copy(k, newPath, StandardCopyOption.REPLACE_EXISTING);
+//					Files.move(k, newPath, StandardCopyOption.REPLACE_EXISTING);
+				} catch (IOException e) {
+					System.err.println(e);
+					System.exit(0);
+				}
+			}
+		});
+	}
+
+	private static void addPrefix(Stream<Path> list) {
+		list.forEach((k) -> {
+			if (!Files.isDirectory(k)) {
+				final String fileName = k.toString();
+				final String newFileName = prefix + fileName.substring(fileName.lastIndexOf(File.separator) + 1);
+				Path newPath = new File(destLocation, newFileName).toPath();
+
+				// logging
+				logger.debug("{} -> {}", k.toString(), newPath.toString());
+
+				try {
+					Files.copy(k, newPath, StandardCopyOption.REPLACE_EXISTING);
+//					Files.move(k, newPath, StandardCopyOption.REPLACE_EXISTING);
+				} catch (IOException e) {
+					System.err.println(e);
+					System.exit(0);
+				}	
+			}
+		});
+	}
 }
