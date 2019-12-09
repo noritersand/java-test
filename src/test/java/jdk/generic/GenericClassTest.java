4d70cf8eaaabee5d5beadd7084e0dc32dd6473c0
noritersand <noritersand@gmail.com>
2019-12-09 17:21:12 +0900

    update

--- src/test/java/jdk/generic/GenericClassTest.java
+++ src/test/java/jdk/generic/GenericClassTest.java
@@ -2,20 +2,33 @@ package jdk.generic;
 
 import java.math.BigDecimal;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * 
+ * 제네릭 클래스 테스트 유닛.
  * 
  * @since 2017-07-27
  * @author fixalot
  */
 public class GenericClassTest {
+	@SuppressWarnings("unused")
+	private static final Logger logger = LoggerFactory.getLogger(GenericClassTest.class);
+	
+	@Test
+	public void testNormalGeneric() {
+		NormalGeneric<String> gen = new NormalGeneric<>();
+		gen.setName("Waldo");
+		Assert.assertEquals("Waldo", gen.getName());
+	}
+	
 	@Test
 	@SuppressWarnings("unused")
-	public void shouldSuccess() {
-		CustomGeneric<BigDecimal> gen = new CustomGeneric<>(); // correct
-		CustomGeneric<LittleDecimal> gen2 = new CustomGeneric<>(); // correct
+	public void testRestrictedGeneric() {
+		RestrictedGeneric<BigDecimal> gen = new RestrictedGeneric<>(); // correct
+		RestrictedGeneric<LittleDecimal> gen2 = new RestrictedGeneric<>(); // correct
 	}
 
 	@Test
@@ -24,16 +37,65 @@ public class GenericClassTest {
 		// Bound mismatch: The type Integer is not a valid substitute for the bounded parameter <T extends BigDecimal> of the type
 		// CustomGeneric<T>
 	}
+	
+	@Test
+	public void testGenericMethod() {
+		GenericMethodOnly gen = new GenericMethodOnly();
+		Assert.assertTrue(Integer.class == gen.getType(Integer.valueOf(123)));
+		Assert.assertTrue(String.class == gen.getType("Text"));
+	}
 
-	private class CustomGeneric<T extends BigDecimal> {
+	/**
+	 * 평범한 제네릭 클래스
+	 * 
+	 * @param <T>
+	 * @since 2019-12-09
+	 * @author noritersand
+	 */
+	private class NormalGeneric<T> {
+		private T name;
+
+		public T getName() {
+			return name;
+		}
+
+		public void setName(T name) {
+			this.name = name;
+		}
+	}
+	
+	/**
+	 * 선언 가능한 타입이 제한된 제네릭 클래스.
+	 * 이 경우 BigDecimal을 상속받은 타입만 제네릭 타입으로 선언할 수 있다.
+	 * 
+	 * @param <T>
+	 * @since 2019-12-09
+	 * @author noritersand
+	 */
+	private class RestrictedGeneric<T extends BigDecimal> {
 		// T는 BigDecimal의 자식 클래스만 올 수 있음
 	}
 
 	private class LittleDecimal extends BigDecimal {
+
 		private static final long serialVersionUID = 2718457985045593298L;
 
 		public LittleDecimal(int val) {
 			super(val);
 		}
+	}
+	
+	/**
+	 * 제네릭 메서드만 있는 클래스.
+	 * 
+	 * @since 2019-12-09
+	 * @author noritersand
+	 */
+	private class GenericMethodOnly {
+		
+	    public <T> Class<?> getType(T arg) { // 리턴타입은 String임. <T>가 아니고
+//	        return arg.getClass().toString();
+	    	return arg.getClass();
+	    }
 	}
 }
