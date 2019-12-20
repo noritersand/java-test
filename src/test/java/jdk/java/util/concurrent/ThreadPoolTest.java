a0cc8a3208dc68e92905f07f450e755589e26cd1
fixalot <fixalot84@gmail.com>
2019-12-20 14:51:03 +0900

    update

--- src/test/java/jdk/java/util/concurrent/ThreadPoolTest.java
+++ src/test/java/jdk/java/util/concurrent/ThreadPoolTest.java
@@ -15,7 +15,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * ThreadPool 테스트 유닛
+ * ThreadPool 테스트 유닛<br>
+ * {@link Mythread} 클래스가 중요하며 홀수/짝수는 큰 의미 없다.
  * 
  * @since 2019-12-19
  * @author noritersand
@@ -26,8 +27,8 @@ public class ThreadPoolTest {
 	private int instanceLength = 10;
 
 	/**
-	 * 쓰레드를 여러개 생성해서 메서드를 동시 실행하는 테스트 케이스.<br>
-	 * 출력 결과를 보면 쓰레드 생성 순서와 실행 순서는 상관 없다는 것을 알 수 있다.
+	 * 쓰레드를 여러개 생성해서 메서드를 동시 호출하는 테스트 케이스.<br>
+	 * 출력 결과를 보면 쓰레드 생성 순서와 호출 순서는 상관 없다는 것을 알 수 있다.
 	 * 
 	 * @author noritersand
 	 */
@@ -36,7 +37,6 @@ public class ThreadPoolTest {
 		ThreadPoolExecutor threadPool = null;
 		Collection<Callable<Integer>> threadList = null;
 		int successCount = 0;
-		int failCount = 0;
 		int howManyThread = 10;
 		try {
 			List<Myclass> list = getList();
@@ -47,14 +47,11 @@ public class ThreadPoolTest {
 					Mythread thread = new Mythread(list.get(i));
 					threadList.add(thread);
 				}
+				// invokeAll: threadList를 threadPool에 등록하고 모두 호출한다.
+				// resultList: 쓰레드 실행 결과. 이 예시에서는 Mythread.call()의 반환값이 할당되어 있음.
 				List<Future<Integer>> resultList = threadPool.invokeAll(threadList);
 				for (Future<Integer> future : resultList) {
-					try {
-						successCount += future.get();
-					} catch (Exception e) {
-						failCount++;
-						logger.error(e.toString());
-					}
+					successCount += future.get(); // future.get() 자바독에 따르면 이 메서드를 호출하는 시점에 아직 쓰레드가 실행중일 수도 있음.
 				}
 			}
 		} catch (Exception e) {
@@ -68,23 +65,27 @@ public class ThreadPoolTest {
 				}
 			}
 		}
-		Assert.assertTrue(instanceLength == successCount);
-		Assert.assertTrue(0 == failCount);
+		Assert.assertTrue(getEvenLength() == successCount); // 짝수만큼 성공
+		Assert.assertTrue(getOddLength() == this.instanceLength - successCount); // 홀수만큼 실패
 	}
 
-	private class Myclass {
-		private int idx;
-
-		public Myclass(int idx) {
-			this.idx = idx;
+	/**
+	 * 그냥 instanceLength개 짜리 리스트 반환하는 메서드
+	 * 
+	 * @return
+	 */
+	public List<Myclass> getList() {
+		List<Myclass> list = new LinkedList<Myclass>();
+		for (int i = 0; i < instanceLength; ++i) {
+			list.add(new Myclass(i));
 		}
-
-		@Override
-		public String toString() {
-			return String.valueOf(idx) + "번 째 인스턴스";
-		}
+		return list;
 	}
-
+	
+	/**
+	 * 쓰레드가 실행할 {@link Callable}의 구현체. 중요.<br>
+	 * {@link ThreadPoolTest}에서만 참조하고 있어서 private으로 설정함.
+	 */
 	private class Mythread implements Callable<Integer> {
 		private Myclass var;
 
@@ -100,25 +101,61 @@ public class ThreadPoolTest {
 			return runThread();
 		}
 
+		/**
+		 * 쓰레드로 실행할 내용이 위치하는 메서드.
+		 * 
+		 * @return
+		 * @author noritersand
+		 */
 		public Integer runThread() {
-			boolean success = true;
-
 			// 두 썸띵
-			logger.debug(String.valueOf(this.var));
+			final int idx = this.var.getIdx();
+			logger.debug(String.valueOf(idx) + "번 째 인스턴스");
 
-			if (success) {
+			// 짝수는 성공, 홀수는 실패하게 함
+			if (idx % 2 == 0) {
 				return 1; // 성공
+			} else {
+				return 0; // 패실
 			}
-			throw new RuntimeException(); // 패실
+//			throw new RuntimeException(); // 굳이 예외를 발생시킬 필요가 있나?
 		}
-
 	}
+	
+	/**
+	 * 그냥 POJO
+	 */
+	private class Myclass {
+		private int idx;
 
-	public List<Myclass> getList() {
-		List<Myclass> list = new LinkedList<Myclass>();
-		for (int i = 0; i < instanceLength; ++i) {
-			list.add(new Myclass(i));
+		public Myclass(int idx) {
+			this.idx = idx;
 		}
-		return list;
+
+		public int getIdx() {
+			return idx;
+		}
+	}
+	
+	/**
+	 * 짝수 몇 개?
+	 */
+	private int getEvenLength() {
+		int length = 0;
+		for (int i = 0; i < this.instanceLength; ++i) {
+			length += i % 2 == 0 ? 1 : 0;
+		}
+		return length;
+	}
+	
+	/**
+	 * 홀수 몇 개?
+	 */
+	private int getOddLength() {
+		int length = 0;
+		for (int i = 0; i < this.instanceLength; ++i) {
+			length += i % 2 == 0 ? 1 : 0;
+		}
+		return length;
 	}
 }
