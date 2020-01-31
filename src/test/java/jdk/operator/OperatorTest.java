8526d29b0e0a497ae93161676e1691340b93529b
fixalot <fixalot84@gmail.com>
2020-01-31 16:55:34 +0900

    add assignment operator test case

--- src/test/java/jdk/operator/OperatorTest.java
+++ src/test/java/jdk/operator/OperatorTest.java
@@ -14,6 +14,38 @@ import org.slf4j.LoggerFactory;
 public class OperatorTest {
 	private static final Logger logger = LoggerFactory.getLogger(OperatorTest.class);
 
+	/**
+	 * 할당 연산자<sup>assignment operator</sup> 테스트
+	 * 
+	 * @author noritersand
+	 */
+	@Test
+	public void assignmentOperator() {
+		// 할당
+		int n = 1;
+		Assert.assertEquals(1, n);
+		
+		// 산술 연산 후 할당
+		n += 2;
+		Assert.assertEquals(3, n);
+		n -= 1;
+		Assert.assertEquals(2, n);
+		n *= 5;
+		Assert.assertEquals(10, n);
+		n /= 2;
+		Assert.assertEquals(5, n);
+		
+		// 변수 하나 이상을 한 번에 초기화
+		int num = n = 9 + 1;
+		Assert.assertEquals(10, num);
+		Assert.assertEquals("20", String.valueOf(num = 20));
+		
+		// 좀 더 많이 해볼까?
+		int a, b, c, d, e;
+		a = b = c = d = e = 100; // 다섯개를 한 번에 조로록
+		Assert.assertTrue(a == b && b == c && c == d && d == e && e == 100); // 당연히 다섯 변수의 값은 같음
+	}
+	
 	@Test
 	public void negativeSign() {
 		Assert.assertTrue(-3 < 0);
