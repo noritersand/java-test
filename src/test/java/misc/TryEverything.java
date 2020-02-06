9876b09179890f5a150a1f44c2d14f25759dbd51
fixalot <fixalot84@gmail.com>
2020-02-06 09:40:21 +0900

    update

--- src/test/java/misc/TryEverything.java
+++ src/test/java/misc/TryEverything.java
@@ -7,7 +7,25 @@ public class TryEverything {
 	@SuppressWarnings("unused")
 	private static final Logger logger = LoggerFactory.getLogger(TryEverything.class);
 
-	public static void main(String[] args) {
+	public static void main(String[] arg) {
+		int a = 3;
+		int A = a * 2 - 1;
+		int K = 1;
+		int B = a - 1;
 
+		for (int i = 1; i <= A; i++) {
+			if (i <= a) {
+				for (int k = 0; k < K; k++) {
+					System.out.print("*");
+				}
+			} else {
+				for (int k = 0; k < B; k++) {
+					System.out.print("*");
+				}
+				B--;
+			}
+			K++;
+			System.out.print("\n");
+		}
 	}
 }
