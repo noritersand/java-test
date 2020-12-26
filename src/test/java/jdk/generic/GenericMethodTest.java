c54cff9d2978155f0a68fd967b78d1383ac2e354
noritersand <noritersand@gmail.com>
2020-12-27 00:34:56 +0900

    update about wildcard parameterized type

--- src/test/java/jdk/generic/GenericMethodTest.java
+++ src/test/java/jdk/generic/GenericMethodTest.java
@@ -1,8 +1,12 @@
 package jdk.generic;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 
@@ -11,23 +15,40 @@ import org.junit.Test;
  * @author fixalot
  */
 public class GenericMethodTest {
+	private static final Logger logger = LoggerFactory.getLogger(GenericMethodTest.class);
+	
 	@Test
 	public void shouldSuccess() {
 		CustomGeneric gen = new CustomGeneric();
+		
 		gen.getSome(new LittleDecimal(123)); // correct
+		
+		List<LittleDecimal> list = Arrays.asList(new LittleDecimal[] { new LittleDecimal(123) });
+		gen.getSome2(list); // correct
 	}
 
 	@Test
 	public void shouldCompileError() {
+//		CustomGeneric gen = new CustomGeneric();
+		
 //		gen.getSome("123"); // wrong
 		// The method getSome(T) in the type GenericMethodTest.CustomGeneric is not applicable for the arguments (String)
+		
 //		gen.getSome(123); // wrong
 		// The method getSome(T) in the type GenericMethodTest.CustomGeneric is not applicable for the arguments (int)
+		
+//		List<Integer> list = Arrays.asList(new Integer[] { Integer.valueOf(123) });
+//		gen.getSome2(list); // wrong
+		// The method getSome2(List<? super GenericMethodTest.TinyDecimal>) in the type GenericMethodTest.CustomGeneric is not applicable for the arguments (List<Integer>)
 	}
 
 	private class CustomGeneric {
 		public <T extends BigDecimal> void getSome(T number) {
-			System.out.println(number);
+			logger.debug("{}", number);
+		}
+		
+		public <T extends BigDecimal> void getSome2(List<? super TinyDecimal> number) {
+			logger.debug("{}", number);
 		}
 	}
 
@@ -35,6 +56,14 @@ public class GenericMethodTest {
 		private static final long serialVersionUID = 2718457985045593298L;
 
 		public LittleDecimal(int val) {
+			super(val);
+		}
+	}
+	
+	private class TinyDecimal extends LittleDecimal {
+		private static final long serialVersionUID = -6691378068791156837L;
+
+		public TinyDecimal(int val) {
 			super(val);
 		}
 	}
--- src/test/java/jdk/java/util/CollectionsTest.java
+++ src/test/java/jdk/java/util/CollectionsTest.java
@@ -24,4 +24,31 @@ public class CollectionsTest {
 		List<Integer> list = Arrays.asList(6, 3, 1, 56, 99, 2, 41, 27, 54, 3);
 		Assert.assertEquals(Integer.valueOf(99), Collections.max(list));
 	}
+
+	/**
+	 * <pre>
+	 * An instantiation of a generic type where the type argument is a wildcard (as opposed to a concrete type). A wildcard parameterized
+	 * type is an instantiation of a generic type where at least one type argument is a wildcard. Examples of wildcard parameterized types
+	 * are Collection<?> , List<? extends Number> , Comparator<? super String> and Pair<String,?> . A wildcard parameterized type denotes a
+	 * family of types comprising concrete instantiations of a generic type. The kind of the wildcard being used determines which concrete
+	 * parameterized types belong to the family. For instance, the wildcard parameterized type Collection<?> denotes the family of all
+	 * instantiations of the Collection interface regardless of the type argument. The wildcard parameterized type List<? extends Number>
+	 * denotes the family of all list types where the element type is a subtype of Number . The wildcard parameterized type Comparator<?
+	 * super String> is the family of all instantiations of the Comparator interface for type argument types that are supertypes of String .
+	 * 
+	 * A wildcard parameterized type is not a concrete type that could appear in a new expression. A wildcard parameterized type is similar
+	 * to an interface type in the sense that reference variables of a wildcard parameterized type can be declared, but no objects of the
+	 * wildcard parameterized type can be created. The reference variables of a wildcard parameterized type can refer to an object that is
+	 * of a type that belongs to the family of types that the wildcard parameterized type denotes.
+	 * </pre>
+	 * 
+	 * {@linkplain http://www.angelikalanger.com/GenericsFAQ/FAQSections/ParameterizedTypes.html#WIldcard+Instantiations}
+	 * 
+	 * @author noritersand
+	 */
+	@Test
+	public void testWildcardParameterizedType() {
+		List<? super String> strList = Arrays.asList(new String[] { "a", "b" });
+		Assert.assertTrue(strList.contains("a"));
+	}
 }
