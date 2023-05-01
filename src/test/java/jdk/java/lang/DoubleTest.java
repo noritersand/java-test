d3f9c8a6cbe438b7378df985f91f928404795d10
fixalot84 <fixalot@anyssign.com>
2023-05-01 21:57:38 +0900

    Update

---
+++ src/test/java/jdk/java/lang/DoubleTest.java
@@ -0,0 +1,26 @@
+package jdk.java.lang;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DoubleTest {
+
+    @Test
+    public void handleDecimalDigits() {
+        int portion = 18;
+        int total = 26;
+        double result = ((double) portion / total) * 100;
+        assertEquals(69.23076923076923, result);
+        assertEquals(69, Math.floor(result));
+
+        BigDecimal bd = new BigDecimal(result);
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        double roundedValue = bd.doubleValue();
+        assertEquals(69.23, roundedValue);
+
+    }
+}
--- src/test/java/jdk/java/util/ListTest.java
+++ src/test/java/jdk/java/util/ListTest.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * {@link List} 테스트
