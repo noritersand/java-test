f341511ba9259829a51c7c403a549fb587cce500
fixalot84 <fixalot@anyssign.com>
2023-08-08 14:34:05 +0900

    Update

--- src/test/java/builtin/heritance/ExtendTest.java
+++ src/test/java/builtin/heritance/ExtendTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
-public class HeritanceTest {
+public class ExtendTest {
 
     @Test
     void test() {
--- src/test/java/builtin/statement/CastingTest.java
+++ src/test/java/builtin/statement/CastingTest.java
@@ -3,7 +3,7 @@ package builtin.statement;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * 캐스팅 테스트
@@ -22,11 +22,11 @@ public class CastingTest {
         int intV = 1;
 //        Double doubleValue = integerValue; // 이 코드는 컴파일 에러가 발생함
         double doubV = intV; // 캐스팅 표현식 없이 타입 캐스팅 가능
-        assertEquals(1.0, doubV);
+        assertThat(doubV).isEqualTo(1.0);
 
         double doubV2 = 5.5;
         int intV2 = (int) doubV2; // 이 경우엔 생략할 수 없음
-        assertEquals(5, intV2);
+        assertThat(intV2).isEqualTo(5);
     }
 
     /**
@@ -36,43 +36,58 @@ public class CastingTest {
     void castWithNull() {
         Object imnotnotnull = null;
         String string = (String) imnotnotnull;
-        assertNull(string);
+        assertThat(string).isNull();
 
         Integer integer = (Integer) imnotnotnull;
-        assertNull(integer);
+        assertThat(integer).isNull();
     }
 
     @Test
     void upCasting() {
         Child c = new Child();
         Parent p = c;
-        assertEquals("Child", p.getName());
+        assertThat(p.getName()).isEqualTo("Child");
 //		p.play(); // undefined method
+
+
     }
 
     @Test
     void downCasting() {
+        Child c1 = new Child();
+        c1.setValue(65536);
+        Parent p1 = (Parent) c1;
+//        p1.getValue(); // Cannot resolve method 'getValue' in 'Parent'
+        // 부모는 없는 getValue() 메서드는 가려진 상태가 됨. 그렇다고 인스턴스 내부의 값이 사라지는 건 아님
+        // 그래서 다운캐스팅을 다시 하면
+        Child c2 = (Child) p1;
+        assertThat(c2.getValue()).isEqualTo(65536);
+
         // 요로케는 안됨.
-        assertThrows(ClassCastException.class, () -> {
+        assertThatThrownBy(() -> {
             Parent p = new Parent();
             @SuppressWarnings("unused")
             Child c = (Child) p;
-        });
+        }).isInstanceOf(ClassCastException.class).hasMessage("class builtin.statement.CastingTest$Parent cannot be cast " +
+                "to class builtin.statement.CastingTest$Child (builtin.statement.CastingTest$Parent and builtin.statement.CastingTest$Child are in unnamed module of loader 'app')");
 
         // 요로케는 됨.
         Parent p = new Child();
         Child c = (Child) p;
 
-        assertEquals("Child", c.getName());
+        assertThat(c.getName()).isEqualTo("Child");
         c.play();
     }
+
     private class Parent {
         public String getName() {
             return "Parent";
         }
 
     }
+
     private class Child extends Parent {
+        private int value;
 
         public void play() {
             log.debug("uoo-hoo!");
@@ -82,5 +97,12 @@ public class CastingTest {
             return "Child";
         }
 
+        public int getValue() {
+            return this.value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
     }
 }
