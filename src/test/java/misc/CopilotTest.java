4399ba7fae75fc64dddb8abc3b7d4683a31809dc
fixalot <fixalot@anyssign.com>
2023-12-17 14:47:40 +0900

    Update

--- src/test/java/misc/CopilotTest.java
+++ src/test/java/misc/CopilotTest.java
@@ -10,6 +10,9 @@ public class CopilotTest {
      * <li>코파일럿 발동: alt + \
      * <li>코파일럿 제안 선택: tab
      * <li>자동 완성 제안 창 보기: Find Action -> Open GitHub Copilot
+     * <li>자동 완성 제안 창 닫기: esc
+     * <li>제안을 단어 단위로 적용하기: ctrl + right
+     * <li>제안을 줄 단위로 적용하기: ctrl + alt + right
      */
     public static void main(String[] args) {
         printGugudan();
@@ -19,10 +22,6 @@ public class CopilotTest {
      * 구구단 출력
      */
     private static void printGugudan() {
-        for (int i = 2; 10 > i; i++) {
-            for (int j = 1; 10 > j; j++) {
-                log.info("{} * {} = {}", i, j, i * j);
-            }
-        }
+        
     }
 }
