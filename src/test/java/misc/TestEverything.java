28c0e0b7ca8eb338f6f85adcef15347229da1567
fixalot <fixalot@a-na.me>
2024-08-05 14:11:00 +0900

    Update

--- src/test/java/misc/TestEverything.java
+++ src/test/java/misc/TestEverything.java
@@ -2,22 +2,19 @@ package misc;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Arrays;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.*;
 
 @Slf4j
 public enum TestEverything {
-	;
+    ;
 
-    public static void main(String[] arg) {
-        int[] numArr = new int[5];
-        for (int i = 0; i < numArr.length; i++) {
-            numArr[i] = i + 1;
-        }
-
-        String str = Arrays.toString(numArr);
-        System.out.println(str);
-        System.out.println(str.substring(3, 6));
-        System.out.println(str.substring(0, 4));
-        System.out.println(str.substring(5));
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime plus = now.plus(78, ChronoUnit.HOURS);
+        LocalDateTime plus1 = plus.plus(55, ChronoUnit.MINUTES);
+        log.debug("{}", plus1);
     }
 }
